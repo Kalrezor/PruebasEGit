6b08bad588897fcdd4b3aed202ede7e2d03726a7
Alejandro <alejandrocaloto1@gmail.com>
2025-03-07 13:03:06 +0100

    Primer commit desde rama1

--- src/PruebasEGit/PruebasEGit.java
+++ src/PruebasEGit/PruebasEGit.java
@@ -5,6 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas EGit");
 		System.out.println("Primer Cambio");
+		System.out.println("cambio desde rama1");
 	}
 
 }
